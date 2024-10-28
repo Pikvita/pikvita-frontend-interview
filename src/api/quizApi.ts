ca36f30c50e26769021d66b0cd192a01df9a1336
Danish Zayan <danishzayan6@gmail.com>
2024-10-28 17:25:14 +0530

    Update .env and add quizApi.ts - implement the api for data fetching.

---
+++ src/api/quizApi.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const baseUrl = import.meta.env.VITE_QUIZ_BASEURL
+const apiKey = import.meta.env.VITE_QUIZ_API_KEY
+
+export const fetchQuizQuestions = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/questions`, {
+      params: {
+        apiKey,
+        category: 'code',
+        difficulty: 'Easy',
+        limit: 10,
+        tags: 'JavaScript',
+      },
+    })
+    console.log('Quiz questions=>', response.data)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching quiz questions:', error)
+    throw error
+  }
+}
