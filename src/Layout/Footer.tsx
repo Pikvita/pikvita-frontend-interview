14ea2168f382b1142a67bcbb5b46f18928021bab
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-11-02 22:51:38 +0530

    add header/footer

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,5 +1,15 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import { Section } from '../components/Section'
 
-export default Footer;
+const Footer = () => {
+  return (
+    <footer className="bg-black">
+      <Section as={'div'} className="h-[40rem] py-20">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-xl text-slate-500">Footer</h1>
+        </div>
+      </Section>
+    </footer>
+  )
+}
+
+export default Footer
