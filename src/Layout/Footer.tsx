021a69de3109a2782c618978a63692019e88b5cf
dhanyabad11 <dhanyabadbehera@gmail.com>
2024-11-04 18:45:46 +0530

    add basic layout components (Header, Aside, Footer)

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,5 +1,11 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import React from 'react'
 
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Quiz Application. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,5 +1,26 @@
-const Header = () => {
-  return <div>Header</div>;
-};
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default Header;
+export default function Header() {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Quiz Application</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/quiz" className="hover:underline">
+                Quiz
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
